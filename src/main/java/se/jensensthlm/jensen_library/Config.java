34959386b08e30ff6716c4967da8252c6c2f81b1
Luis Paz <lpaz8806@gmail.com>
2022-05-30 13:16:15 +0200

    config and schema information boilerplate

---
+++ src/main/java/se/jensensthlm/jensen_library/Config.java
@@ -0,0 +1,7 @@
+package se.jensensthlm.jensen_library;
+
+public final class Config {
+    public static final String DB_CONNECTION = "jdbc:mariadb://localhost:3306/jensen_library";
+    public static final String DB_USER = "your user name goes here";
+    public static final String DB_PASS = "your password goes here";
+}
--- src/main/java/se/jensensthlm/testing_helpers/SchemaInformation.java
+++ src/main/java/se/jensensthlm/testing_helpers/SchemaInformation.java
@@ -1,8 +1,67 @@
 package se.jensensthlm.testing_helpers;
 
+import se.jensensthlm.jensen_library.Config;
+
+import java.io.Closeable;
+import java.sql.*;
+
 /**
  *
  */
-public class SchemaInformation {
+public class SchemaInformation implements Closeable {
+    private Connection db;
+    private String dbName = null;
 
+    public SchemaInformation() {
+        try {
+            db = DriverManager.getConnection(
+                    Config.DB_CONNECTION,
+                    Config.DB_USER,
+                    Config.DB_PASS
+            );
+        } catch (Exception e) {
+            System.out.printf("Error occurred: %s", e.getMessage());
+        }
+    }
+
+    public String getDbName() {
+        if (dbName == null) {
+            dbName = fetchDbName();
+        }
+        return dbName;
+    }
+
+    public void close() {
+        try {
+            db.close();
+        } catch (SQLException e) {
+            return;
+        }
+    }
+
+    /*
+    Implement here:
+
+    getPrimaryKeys()
+
+    getForeignKeys()
+    
+    getTypes()
+    */
+
+
+    private String fetchDbName() {
+        try {
+            ResultSet rs = executeQuery("SELECT DATABASE() as `db` FROM DUAL");
+            rs.next();
+            return rs.getString("db");
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    private ResultSet executeQuery(String query, String... params) throws SQLException {
+        Statement stmt = db.createStatement();
+        ResultSet rs = stmt.executeQuery(String.format(query, params));
+        return rs;
+    }
 }
